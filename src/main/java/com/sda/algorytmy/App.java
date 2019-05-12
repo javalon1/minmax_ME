c13db50ed4249e00bdce76933f6f0cbcb20478d3
Mateusz Erdman/Matek <mateusz.erdman@hotmail.com>
2019-05-12 12:59:02 +0100

    Rozwiazanie minmax

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -8,6 +8,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+     int tab[] = {4,6,2,1,3};
+     int res = max(tab);
+     int res1 = min(tab);
+     System.out.println("Max el: " +res);
+        System.out.println("Min el: " +res1);
+
     }
+        public static int max(int[] tab){
+        int max =tab[0];
+        for(int i =0; i < tab.length; i++){
+            if(max < tab[i]){
+            max = tab[i];
+            }
+        }
+        return max;
+        }
+        public static int min(int[] tab){
+        int min = tab[0];
+        for(int i=0; i < tab.length; i++){
+            if(min>tab[i]){
+                min = tab[i];
+            }
+        }
+        return min;
+        }
+
 }
